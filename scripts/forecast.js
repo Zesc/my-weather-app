25558284aeaad07031d81e9038d8c3806c55978c
Zesc <jeddak.cesc@gmail.com>
2020-08-18 22:07:18 -0700

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+//jshint esversion:8
+
+const key = 'yVZMLVsuLTWbubO3AMAMLGFF9BclRua9';
+
+// get weather information
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+// get city information
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// getCity('lima').then(data => {
+//   return getWeather(data.Key);
+// }).then(data => console.log(data))
+//   .catch(err => console.log(err));
